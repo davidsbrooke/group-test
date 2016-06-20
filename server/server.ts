b1ae1a01520445028fcb84b947cc5944a69ee5e3
Yassin <Yassin@Yassins-MacBook-Pro.local>
2016-06-20 11:19:40 -0700

    nothing

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,10 @@
 import * as express from 'express';
+import * as mongoose from 'mongoose';
 
 const app = express();
 
-app.listen(3000), () => {
+app.listen(3000, () => {
 
-console.log('Server is listening on localhost:4000');
+console.log('Server is listening on localhost:3000');
 
 });
